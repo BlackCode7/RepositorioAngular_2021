d341f14b78a2f54fa066cb0c63a0e76871f362ce
Anderson <BlackCode7@outlook.com>
2021-11-17 21:21:52 -0300

    Iniciando estudo com livros em inglês

---
+++ BookAngularCookbook/cookbookangular/src/app/notifications-manager/notifications-manager.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-manager',
+  templateUrl: './notifications-manager.component.html',
+  styleUrls: ['./notifications-manager.component.css']
+})
+export class NotificationsManagerComponent implements OnInit {
+
+  
+
+  @Input() count = 0;
+  constructor() { }
+  ngOnInit(): void {}
+
+  addNotification(){this.count++;}
+  removeNotification(){
+    if (this.count == 0){return;}
+    this.count--;
+  }
+  resetCount(){this.count = 0;}
+}
